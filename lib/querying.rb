e7035d709dfb5a2a6a3fd765dcb375045b574cf2
Heather Kirk <hskirk@gmail.com>
2019-04-01 17:44:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,7 @@ def select_series_title_with_most_human_characters
   "SELECT title FROM series
   JOIN characters 
   ON series.id = characters.series_id
-  WHERE species = 'human'
+  WHERE characters.species = 'human'
   GROUP BY title
   ORDER BY COUNT species DESC LIMIT 1;
   "
