bfaeb501547aabb692da0477eddec5cd904d5f20
Heather Kirk <hskirk@gmail.com>
2019-04-01 17:26:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,5 +36,8 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name FROM characters
-  JOIN books"
+  JOIN books
+  ON characters.id = books.character_id
+  GROUP BY characters.name 
+  ORDER BY COUNT books DESC LIMIT "
 end
